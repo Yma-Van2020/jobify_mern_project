aee7dec32df9a59b8faf5310d6265b9894b4b8c0
Katelyn Ma <katelynvan152@gmail.com>
2023-04-09 10:01:55 -0700

    add validator and error handling

---
+++ models/User.js
@@ -0,0 +1,41 @@
+import mongoose, { mongo } from 'mongoose';
+import validator from 'validator'
+
+const UserSchema = new mongoose.Schema({
+  name : {
+    type: String,
+    required: [true,  ' Pleased provide name'],
+    minlength: 3,
+    maxlength: 20,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true,  ' Pleased provide email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please pro'
+    },
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true,  ' Pleased provide password'],
+    minlength: 6
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    minlength: 3,
+    maxlength: 20,
+    default: 'last name'
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: 'my city'
+  }
+})
+
+export default mongoose.model('User', UserSchema)
