76e189676fa0c40b3df41db1e86d067a46fc2793
Katelyn Ma <katelynvan152@gmail.com>
2023-04-09 22:03:50 -0700

    JWT setup

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 import mongoose, { mongo } from 'mongoose';
 import validator from 'validator'
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
 const UserSchema = new mongoose.Schema({
   name : {
     type: String,
@@ -21,7 +22,8 @@ const UserSchema = new mongoose.Schema({
   password: {
     type: String,
     required: [true,' Pleased provide password'],
-    minlength: 6
+    minlength: 6,
+    select: false
   },
   lastName: {
     type: String,
@@ -42,6 +44,11 @@ UserSchema.pre('save', async function() {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt)
 })
+
+UserSchema.methods.createJWT = function () {
+  // returns a signed JWT as a string
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, { expiresIn: '1d'})
+}
 
 
 export default mongoose.model('User', UserSchema)
