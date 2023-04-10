ce4f7f0048f779c28caab8d9803f9c8283231184
Katelyn Ma <katelynvan152@gmail.com>
2023-04-09 17:54:48 -0700

    hash password

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 import mongoose, { mongo } from 'mongoose';
 import validator from 'validator'
-
+import bcrypt from 'bcryptjs'
 const UserSchema = new mongoose.Schema({
   name : {
     type: String,
@@ -37,5 +37,11 @@ const UserSchema = new mongoose.Schema({
     default: 'my city'
   }
 })
+
+UserSchema.pre('save', async function() {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 
 export default mongoose.model('User', UserSchema)
