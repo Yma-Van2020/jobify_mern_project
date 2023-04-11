b94cc0e19742002277a1a31fd6f6b651adbcc906
Katelyn Ma <katelynvan152@gmail.com>
2023-04-11 12:03:05 -0700

    nested pages and protected route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import {
-  Dashboard,
   Error,
   Register,
-  Landing
+  Landing,
+  ProtectedRoute
 } from './pages'
+import {AddJob, AllJobs, Profile, Stats, SharedLayout} from './pages/Dashboard'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={
+          <ProtectedRoute>
+            <SharedLayout/>
+          </ProtectedRoute>
+          }>
+            <Route index element={<Stats/>}/>
+            <Route path="all-jobs" element={<AllJobs/>}/>
+            <Route path="add-job" element={<AddJob/>}/>
+            <Route path="profile" element={<Profile/>}/>
+          </Route>
           <Route path="/register" element={<Register />} />
           <Route path="/landing" element={<Landing />} />
           <Route path="*" element={<Error />} />
--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,11 +1,12 @@
-import Dashboard from "./Dashboard";
+
 import Error from "./Error";
 import Register from "./Register";
 import Landing from "./Landing";
+import ProtectedRoute from "./ProtectedRoute";
 
 export {
-  Dashboard,
   Error,
   Register,
+  ProtectedRoute,
   Landing
 }
