de898849defd8ae595795b373a308d475f59dc6e
Katelyn Ma <katelynvan152@gmail.com>
2023-05-01 07:23:29 -0700

    setup monthly applications-frontend

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,6 +1,20 @@
+import React, { useState } from 'react'
+import BarChart from './BarChart.js'
+import AreaChart from './AreaChart.js'
+import { useAppContext } from '../context/appContext.js'
+import Wrapper from '../assets/wrappers/ChartsContainer.js'
+
 const ChartsContainer = () => {
+  const [barChart, setBarChart] = useState(true)
+  const {monthlyApplications:data} = useAppContext()
   return (
-    <div>ChartsContainer</div>
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart? 'Area Chart':'Bar Chart'}
+      </button>
+      {barChart? <BarChart data={data}/>:<AreaChart data={data}/>}
+    </Wrapper>
   )
 }
 export default ChartsContainer
--- client/src/components/StatItem.js
+++ client/src/components/StatItem.js
@@ -1,6 +1,15 @@
-const StatsItem = () => {
+import Wrapper from '../assets/wrappers/StatItem'
+
+const StatsItem = ({ count, title, icon, color, bcg }) => {
   return (
-    <div>single stat item</div>
+    <Wrapper color={color} bcg={bcg}>
+      <header>
+        <span className='count'>{count}</span>
+        <span className='icon'>{icon}</span>
+      </header>
+      <h5 className='title'>{title}</h5>
+    </Wrapper>
   )
 }
+
 export default StatsItem
